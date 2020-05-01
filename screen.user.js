d6c9a358faef29a08ed3f82a0f6c88e0cefaa04b
tyanbara <64423613+tyanbara@users.noreply.github.com>
2020-05-01 17:14:44 +0900

    ダメそう

--- screen.user.js
+++ screen.user.js
@@ -6,30 +6,45 @@
 // @author       You
 // @match        https://web.screen.rakuten.co.jp/app
 
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+
 // @downloadURL https://github.com/tyanbara/js_study/raw/master/screen.user.js
 // @updateURL   https://github.com/tyanbara/js_study/raw/master/screen.user.js
 // @grant        none
 // ==/UserScript==
 
 
-window.onload = function () {
-
-    console.log('start');
-    var btm_lst = document.getElementsByClassName('f4 itemButton');
-    var btm_lst_n = btm_lst.length;
-
-    var hold_num = -1;
-    for (var i; i < btm_lst.length; i++) {
-        console.log(btm_lst.length[i].textContent);
-        if ('1ポイント' == btm_lst.length[i].textContent) {
-            hold_num = i;
-            break;
-        }
+// $(document).ready(function () {
+    jQuery(window).on('load', function ($) {
+        //    function  {
+    
+        setInterval(function () {
+            console.log('start');
+            var btm_lst = document.getElementsByClassName('f4 itemButton');
+            var btm_lst_n = btm_lst.length;
+    
+             var test = document.getElementsByClassName('card flex flex-column width100');
+    
+            var hold_num = -1;
+            for (var i=0; i < btm_lst.length; i++) {
+                console.log(btm_lst[i].textContent);
+                if ('1ポイント' == btm_lst[i].textContent) {
+                    hold_num = i;
+                    break;
+                }
+            }
+    
+            if (hold_num != -1) {
+                console.log('click');
+                // var event = document.createEvent("MouseEvents"); // イベントオブジェクトを作成
+                // event.initEvent("click", false, true); // イベントの内容を設定
+                // btm_lst[hold_num].dispatchEvent(event);
+                // btm_lst[hold_num].click();
+                test[hold_num].click();
+            }
+        }, 5000); // setInterval
     }
-
-    if (hold_num > 0) {
-        var event = document.createEvent("MouseEvents"); // イベントオブジェクトを作成
-        event.initEvent("click", false, true); // イベントの内容を設定
-        btm_lst[hold_num].dispatchEvent(event);
-    }
-}
+    )
+    
+    
+    
