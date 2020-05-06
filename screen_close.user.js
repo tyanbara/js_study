c562cd8182aff1e5b6858cc422d310b9d3c2ec3e
tyanbara <64423613+tyanbara@users.noreply.github.com>
2020-05-07 00:10:07 +0900

    アドレス修正

--- screen_close.user.js
+++ screen_close.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         screen_close
 // @namespace    http://tampermonkey.net/
-// @version      0.11.5
+// @version      0.11.6
 // @description  try to take over the world!
 // @author       You
 
 // @match       https://02.api.screen.rakuten.co.jp/1/adwall*
 // @match       https://pasha.rakuten.co.jp/today*
 // @match       https://screen.rakuten.co.jp/special/app_today/*
-// @match       https://screen.rakuten.co.jp/*adwall*
+// @match       https://screen.rakuten.co.jp/adwall*
 // @match       https://screen.rakuten.co.jp/reward*
 
 // @downloadURL https://github.com/tyanbara/js_study/raw/master/screen_close.user.js
