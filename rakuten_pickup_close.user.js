6adce6517725489fe0f75ac88e4b93fd8c58db07
tyanbara <64423613+tyanbara@users.noreply.github.com>
2020-06-05 23:20:58 +0900

    対象を追加

--- rakuten_pickup_close.user.js
+++ rakuten_pickup_close.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         rakuten_pickup_close
 // @namespace    http://tampermonkey.net/
-// @version      0.11.5
+// @version      0.11.6
 // @description  try to take over the world!
 // @author       You
 
@@ -17,6 +17,7 @@
 // @match        https://www.rakuten-card.co.jp/campaign/sales/ichiba_recommend/
 // @match        https://item.rakuten.co.jp/*scid=wi_ichi_ad_pc_*
 // @match        https://edy.rakuten.co.jp/campaign/other/rakuten/ichiba_recommend
+// @match        https://*.rakuten.co.jp/*scid=we_ich_adsol*
 
 // @downloadURL https://github.com/tyanbara/js_study/raw/master/rakuten_pickup_close.user.js
 // @updateURL   https://github.com/tyanbara/js_study/raw/master/rakuten_pickup_close.user.js
