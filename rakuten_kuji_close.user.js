447a400952d706693afbb9152c3e8e3d6ec7bdf4
tyanbara <64423613+tyanbara@users.noreply.github.com>
2020-12-20 14:23:57 +0900

    バージョンアップ

--- rakuten_kuji_close.user.js
+++ rakuten_kuji_close.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         rakuten_kuji_close
 // @namespace    http://tampermonkey.net/
-// @version      0.11.6
+// @version      0.11.7
 // @description  try to take over the world!
 // @author       You
 
