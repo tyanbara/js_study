1912896de509fe84bf8f39201a56a20dd7ff71b1
tyanbara <64423613+tyanbara@users.noreply.github.com>
2022-07-16 13:54:36 +0900

    新規

---
+++ mail_de_point.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         mail_de_point
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://member.pointmail.rakuten.co.jp/box
+
+
+// @downloadURL https://github.com/tyanbara/js_study/raw/master/mail_de_point.user.js
+// @updateURL   https://github.com/tyanbara/js_study/raw/master/mail_de_point.user.js
+
+// @require      https://github.com/tyanbara/js_study/raw/master/window_close.user.js
+
+// @grant        none
+// ==/UserScript==
+
+
+
+var delay_time = 3 * 1000;
+
+
+
+window.onload = function () {
+	setTimeout(function () {
+		var clickMe = document.getElementsByClassName("pointNotGetCount");
+        if (clickMe[0].length == '0') {
+			console.log('mail_0');
+            // consle.log(clickMe2)
+            // my_window_close();
+        }
+
+		console.log('OK_click');
+        console.log(clickMe[0].length );
+		console.log('OK_click');
+		// console.log(clickMe);
+		// clickMe.dispatchEvent(event);
+		// clickMe.click();
+
+
+	}, delay_time);
+};
+// });
+
